4191814ac99bbe292addbb860c376fe3a337a999
Raiyan109 <kraiyan109@gmail.com>
2022-08-05 05:44:17 +0600

    header animation done

---
+++ src/components/Home/index.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-s.png'
+import './index.scss'
+const Home = () => {
+    return <div className="container home-page">
+        <div className="text-zone">
+            <h1>Hi, <br /> I'm
+                <img src={LogoTitle} alt="" />
+                lobodan
+                <br />
+                Web Developer
+            </h1>
+            <h2>Frontend Developer / JavaScript Expert </h2>
+            <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+        </div>
+    </div>
+}
+
+export default Home
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -5,6 +5,7 @@ import logoS from '../../assets/images/logo-s.png'
 import logoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faGithub, faGoogle, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const SideBar = () => {
     return (
@@ -25,6 +26,29 @@ const SideBar = () => {
                     <FontAwesomeIcon icon={faHome} color="4d4d4e" />
                 </NavLink>
             </nav>
+
+            <ul>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='linkedin.com/in/raiyan-kabir-1bb775229/'>
+                        <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='https://github.com/Raiyan109'>
+                        <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='https://www.facebook.com/profile.php?id=100008869806599'>
+                        <FontAwesomeIcon icon={faFacebook} color='#4d4d4e' />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='nkedin.com/in/raiyan-kabir-1bb775229/'>
+                        <FontAwesomeIcon icon={faGoogle} color='#4d4d4e' />
+                    </a>
+                </li>
+            </ul>
         </div>
     );
 };
